4d20f461c736c1309ae130abefec993020099251
Mikael Mantis <mikael.mantis7@gmail.com>
2023-03-07 00:00:34 -0800

    adds deno deploy configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,16 @@ import react from '@astrojs/react';
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+import deno from "@astrojs/deno";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [
   // Enable Preact to support Preact JSX components.
   preact(),
   // Enable React for the Algolia search component.
   react(), mdx()],
-  site: `https://astro.build`
+  site: `https://astro.build`,
+  output: "server",
+  adapter: deno()
 });
