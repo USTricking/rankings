e79a6572a4982ae01615511eade95ac875a65da1
Mikael Mantis <mikael.mantis7@gmail.com>
2023-05-08 23:59:17 -0700

    parameterizes tier colors

--- src/models/TrickingEvent.ts
+++ src/models/TrickingEvent.ts
@@ -2,3 +2,13 @@ export interface TrickingEvent {
   readonly name: string;
   readonly tier: number;
 }
+
+interface TierGradients {
+  readonly [key: number]: string;
+}
+
+export const tierGradients: TierGradients = {
+  1: 'linear-gradient(135deg, #ffe259 10%, #ffa751 100%)',
+  2: 'linear-gradient(135deg, #cfd9df 10%, #e2ebf0 100%)',
+  3: 'linear-gradient(135deg, #c79081 10%, #dfa579 100%)'
+};
